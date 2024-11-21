54ca5a9b3d7d0dcc2010af0f242157e48cb50466
BoxyPipesnake <felipe_motoss@hotmail.com>
2024-11-21 00:51:37 -0600

    Books Create/Index/New

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts')
 
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
-const bodyParser = require('body-parser')
+const bookRouter = require('./routes/books')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -28,6 +31,7 @@ async function connectDB() {
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`);
