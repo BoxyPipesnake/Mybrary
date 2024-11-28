75293f64a063d38d109585da45f39984b1942c0b
BoxyPipesnake <felipe_motoss@hotmail.com>
2024-11-27 20:36:02 -0600

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    }
+})
+
+authorSchema.pre('deleteOne', { document: true, query: false }, async function (next) {
+    try {
+        // Check if there are books linked to this author
+        const books = await Book.find({ author: this.id });
+        if (books.length > 0) {
+            return next(new Error('This author still has books associated with them'));
+        }
+        next(); // Proceed if no books are found
+    } catch (err) {
+        next(err); // Handle errors during the check
     }
 })
 
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts')
-
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -18,8 +18,11 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+
 
 const mongoose = require('mongoose')
 connectDB().then(() => console.log('Connected to Mongoose')).catch(err => console.log(err));
